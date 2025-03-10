2de744359571e00a5f56f6d8dcb3ee7586f95eaa
DaviMoreira27 <davimsantana2706@gmail.com>
2025-03-09 23:52:36 -0300

    wip(google-config): added initial google bucket config
    Signed-off-by: DaviMoreira27 <davimsantana2706@gmail.com>

--- src/integration/google/google.service.ts
+++ src/integration/google/google.service.ts
@@ -1,4 +1,13 @@
+import { Storage } from '@google-cloud/storage';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class GoogleService {}
+export class GoogleService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly googleStorage: Storage,
+  ) {
+    this.googleStorage.bucket(this.configService.get<string>('googleBucket')!);
+  }
+}
