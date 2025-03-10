e530ff3e5415abdbfe5bad51b3558af84627904b
DaviMoreira27 <davimsantana2706@gmail.com>
2025-03-09 21:59:25 -0300

    chore: scaffold finalized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { NotionService } from './integration/notion/notion.service';
+import { WhatssapService } from './integration/whatssap/whatssap.service';
+import { GoogleService } from './integration/google/google.service';
+import { MessagingService } from './facade/messaging/messaging.service';
+import { StorageService } from './facade/storage/storage.service';
+import { NotesService } from './facade/notes/notes.service';
 import config from './config/config';
 
 @Module({
@@ -10,6 +16,13 @@ import config from './config/config';
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    NotionService,
+    WhatssapService,
+    GoogleService,
+    MessagingService,
+    StorageService,
+    NotesService,
+  ],
 })
 export class AppModule {}
