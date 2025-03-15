74827f87b94f7b9df5ac2e293d759f4f12b18257
DaviMoreira27 <davimsantana2706@gmail.com>
2025-03-14 22:52:41 -0300

    feat: added main app error class

--- src/app.error.ts
+++ src/app.error.ts
@@ -18,4 +18,18 @@ enum ExternalServices {
   NOTION = 'NOTION',
 }
 
-export abstract class MainAppError extends Error {}
+export abstract class MainAppError extends Error implements AppError {
+  public readonly httpCode: HttpStatusCode;
+  public readonly externalServiceError?: ExternalServiceError;
+
+  constructor(message: string, httpCode: HttpStatusCode, externalServiceError?: ExternalServiceError) {
+    super(message);
+    this.httpCode = httpCode;
+    this.externalServiceError = externalServiceError;
+    this.name = this.constructor.name;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
