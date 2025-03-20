f7f4b288764f6f54b29a9d5cefd6ad9fd15d2c8f
DaviMoreira27 <davimsantana2706@gmail.com>
2025-03-19 22:19:26 -0300

    feat: added file and createWriteStream mock functions
    Signed-off-by: DaviMoreira27 <davimsantana2706@gmail.com>

--- src/integration/google-storage/google-storage.service.spec.ts
+++ src/integration/google-storage/google-storage.service.spec.ts
@@ -3,15 +3,42 @@ import { GoogleStorageService } from './google-storage.service';
 import { Bucket, File, Storage } from '@google-cloud/storage';
 import { ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
+import { Writable } from 'stream';
 
 describe('GoogleStorageService', () => {
   // Define all the values that I will be using
   let service: GoogleStorageService;
   let bucketMock: Partial<Bucket>;
   let configServiceMock: Partial<ConfigService>;
+  let file: Partial<File>;
 
   // It will run before each test
   beforeEach(async () => {
+    file = {
+      createWriteStream: jest.fn().mockImplementation((options?: { contentType?: string; resumable?: boolean }) => {
+        const { contentType, resumable = false } = options || {}; // Extraindo os parâmetros com valores padrão
+
+        const stream = new Writable({
+          write(chunk, encoding, callback) {
+            callback();
+          },
+        });
+
+        stream.on = jest.fn((event, callback) => {
+          if (event === 'finish') {
+            setImmediate(callback);
+          }
+          if (event === 'error') {
+            setImmediate(() => callback(new Error('Upload failed')));
+          }
+          return stream;
+        });
+
+        console.log(`Mocked createWriteStream called with: contentType=${contentType}, resumable=${resumable}`);
+        return stream;
+      }),
+    };
+
     // Mocking the Google Storage Bucket class, because its define as partial we only have to inform the 2 methods that we will use
     bucketMock = {
       // getMetadata: an async mocked function that will return an array with a timeCreated object when resolved
@@ -25,6 +52,10 @@ describe('GoogleStorageService', () => {
             { metadata: { timeCreated: '2024-03-12T12:00:00Z' } } as Partial<File>,
           ],
         ]),
+      file: jest
+        .fn()
+        .mockImplementation((filePath: string) => new File(bucketMock as Bucket, filePath))
+        .mockResolvedValue(file),
     };
 
     // Defining the Google Storage class mock, we are saying that for the bucket property
@@ -74,11 +105,23 @@ describe('GoogleStorageService', () => {
 
     service.getAllFiles(startDate, endDate).subscribe({
       next: (files: File[]) => {
-        console.log('FILES', files);
         expect(files.length).toBe(2);
         expect(bucketMock.getMetadata).toHaveBeenCalled();
         expect(bucketMock.getFiles).toHaveBeenCalledWith({ prefix: 'notion/subjects/images/' });
         done();
+      },
+      error: () => {
+        done();
+      },
+    });
+  });
+
+  it('should upload a file to the bucket', (done) => {
+    const fileUrl = 'https://fastly.picsum.photos/id/85/200/300.jpg?hmac=_MELEMGQCalX-bflh-qD89Z5VjdVMfVXD68WblQSLM8';
+    const filePath = 'notion/subjects/images/software-requirements/';
+    service.uploadFile(fileUrl, filePath).subscribe({
+      next: () => {
+        console.log('Sucess');
       },
       error: () => {
         done();
