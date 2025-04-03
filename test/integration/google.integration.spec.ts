9a67bb835f2c31dd39e529cfc52fbf401f9f5487
DaviMoreira27 <davimsantana2706@gmail.com>
2025-04-02 21:35:33 -0300

    fix: fixed not being able to get the env data
    Signed-off-by: DaviMoreira27 <davimsantana2706@gmail.com>

--- test/integration/google.integration.spec.ts
+++ test/integration/google.integration.spec.ts
@@ -1,43 +1,43 @@
-import { Test } from "@nestjs/testing";
-import { GoogleStorageModule } from "src/integration/google-storage/google-storage.module";
-import { GoogleStorageService } from "src/integration/google-storage/google-storage.service";
+import { Storage } from '@google-cloud/storage';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import config from 'src/config/config';
+import { GoogleStorageModule } from 'src/integration/google-storage/google-storage.module';
+import { GoogleStorageService } from 'src/integration/google-storage/google-storage.service';
 
 describe('Google Storage Integration', () => {
+  let app: any;
+  let googleService: GoogleStorageService;
 
-    let app: any;
-    let googleService: GoogleStorageService;
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule.forRoot({ isGlobal: true, load: [config] })],
+      providers: [ConfigService, Storage, GoogleStorageService],
+    }).compile();
+
+    app = module.createNestApplication();
+    googleService = module.get(GoogleStorageService);
+
+    await app.init();
+  });
 
 
-    beforeAll(async () => {
-        const module = await Test.createTestingModule({
-            imports: [GoogleStorageModule],
-        }).compile();
+  it('Should upload a file to the GCP bucket', (done) => {
+    const fileUrl = 'https://fastly.picsum.photos/id/85/200/300.jpg?hmac=_MELEMGQCalX-bflh-qD89Z5VjdVMfVXD68WblQSLM8';
+    const filePath = 'notion/subjects/images/software-requirements/image-04-02-jpg';
 
-        app = module.createNestApplication();
-        googleService = module.get(GoogleStorageService);
-
-        await app.init();
+    googleService.uploadFile(fileUrl, filePath).subscribe({
+      next: (response: string) => {
+        expect(response).toEqual(filePath);
+      },
+      error: (error: Error) => {
+        console.log('Integration test error: ', error.message);
+        done(error);
+      },
+      complete: () => {
+        done();
+      },
     });
-
-    it('Should upload a file to the GCP bucket', (done) => {
-        const fileUrl =
-                'https://fastly.picsum.photos/id/85/200/300.jpg?hmac=_MELEMGQCalX-bflh-qD89Z5VjdVMfVXD68WblQSLM8';
-        const filePath = 'notion/subjects/images/software-requirements/image-04-02-jpg';
-
-
-        googleService.uploadFile(fileUrl, filePath).subscribe({
-            next: (response: string) => {
-                expect(response).toEqual(filePath);
-            },
-            error: (error: Error) => {
-                console.log('Integration test error', error.message);
-                done()
-            },
-            complete: () => {
-                done();
-            }
-        })
-        
-
-    })
-})
+  });
+});
