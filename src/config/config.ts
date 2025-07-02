2abd4e6aae434dfadb5eae49e908b02144b2aedc
DaviMoreira27 <davimsantana2706@gmail.com>
2025-07-01 22:08:44 -0300

    chore: added new env property

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,4 +3,6 @@ import { getEnv } from './config.types';
 export default () => ({
   environment: getEnv(process.env.NODE_ENV),
   port: parseInt(process.env.PORT || '3000', 10),
+  WHATSAPP_VERIFY_TOKEN: process.env.MY_TOKEN ?? 'APPLE',
+  WHATSAPP_ACCESS_TOKEN: process.env.WHATSSAP_ACCESS_TOKEN ?? 'BANANA',
 });
