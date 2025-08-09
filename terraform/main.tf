ed94d68b6fbb43fd7f6bc1b46112c79ac41ec879
DaviMoreira27 <davimsantana2706@gmail.com>
2025-08-09 01:47:55 -0300

    feat: reorganized my architecture

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,31 @@ provider "aws" {
   region = var.region
 }
 
+provider "kubernetes" {
+}
+
+module "network" {
+  source = "./modules/aws-network"
+
+  availability_zones = var.availability_zones
+  environment = var.environment
+  region = var.region
+  base_cidr_block = var.base_cidr_block
+}
+
+module "compute" {
+  source = "./modules/aws-compute"
+
+  availability_zones = var.availability_zones
+  environment = var.environment
+  region = var.region
+}
+
+module "storage" {
+  source = "./modules/aws-storage"
+}
+
+module "k8s" {
+  source = "./modules/k8s/"
+}
 
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.5.0"
     }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
   }
 
   required_version = "~> 1.2"
